86294e267127ec68b13e13f0e28ea04e8a0f8969
zzu-yaaa <jy122032@gmail.com>
2024-05-02 04:36:10 +0900

    feat: 의존성 추가 및 컨트롤러 생성

--- src/main/java/org/example/springjwt/controller/MyController.java
+++ src/main/java/org/example/springjwt/controller/MyController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@ResponseBody
-public class AdminController {
-
-    @GetMapping("/admin")
-    public String adminP(){
-        return "admin Controller";
+public class MyController {
+    @GetMapping("/my")
+    @ResponseBody
+    public String myAPI(){
+        return "my route";
     }
+
 }
